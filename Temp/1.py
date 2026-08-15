e89f7348332c880879c28388d369f2644d2a44c7
WalkUp <3941293269@qq.com>
2026-08-15 10:37:57 +0800

    Merge branch 'main' of https://github.com/WalkUpOfficial/WalkUp

--- Temp/1.py
+++ Temp/1.py
@@ -1,9 +1,16 @@
-input()
-a = list(map(int, input().split(' ')))
-j,o = [],[]
-for i in a:
-    if i % 2 == 0:
-        o.append(i)
-    else:
-        j.append(i)
-print(f'{max(j)} {max(o)}\n{' '.join(list(map(str, sorted(a))))}')
+n, m = map(int, input().split())
+a = []
+
+for i in range(n):
+    x = list(map(int, input().split()))
+    a.append(x)
+
+for i in range(1, m):
+    a[0][i] = a[0][i-1]
+for j in range(1, m):
+    a[j+1][0] = a[j-1][0]
+for i in range(n):
+    for j in range(m):
+        a[i][j] += max(a[i-1][j], a[i][j-1])
+
+print(a[n-1][m-1])
