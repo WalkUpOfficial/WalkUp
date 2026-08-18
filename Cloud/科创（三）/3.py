de82b6ee7bbf1d410808a9daf5f7106f65990097
WalkUp <3941293269@qq.com>
2026-08-18 20:23:04 +0800

    Updated Files

--- Cloud/科创（三）/3.py
+++ Cloud/科创（三）/3.py
@@ -1,4 +1,9 @@
-n = int(input())
-a = []
-for i in range(n):a.append(int(input()))
-for i in range(n):
+for i in range(int(input())):
+    data = int(input())
+    for j in range(2, int(data ** 0.5) + 1):
+        if data % j == 0:
+            print(data // j)
+            break
+    else:
+        if data > 1:print("yes")
+        else:print(data)
